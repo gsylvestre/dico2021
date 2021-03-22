939e5eae371c7df4b6e01bae0b26732222ebc06d
Guillaume <gsylvestre@gmail.com>
2021-03-22 14:34:15 +0100

    module 2 : création de l'accueil + test

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route ("/")
+     */
+    public function home()
+    {
+        return $this->render("main/home.html.twig");
+    }
+
+    /**
+     * @Route ("/test")
+     */
+    public function test()
+    {
+        return $this->render("main/test.html.twig");
+    }
+}
