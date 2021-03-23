3d794209f5153ce74d57b6cdecd0092df6a61fc5
Guillaume <gsylvestre@gmail.com>
2021-03-23 15:33:24 +0100

    module 5 : démo entitymanager et repository

--- src/Controller/TermController.php
+++ src/Controller/TermController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Term;
+use App\Repository\TermRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +14,42 @@ class TermController extends AbstractController
     /**
      * @Route("/tous-les-mots/", name="term_list")
      */
-    public function list(): Response
+    public function list(TermRepository $termRepository): Response
     {
-        return $this->render('term/list.html.twig', [
+        $terms = $termRepository->findAll();
 
+        return $this->render('term/list.html.twig', [
+            "terms" => $terms
         ]);
     }
+
+    /**
+     * @Route("/term/create/demo", name="term_create_demo")
+     */
+    public function createDemo(EntityManagerInterface $entityManager)
+    {
+        //instanciation d'un nouveau terme du dictionnaire
+        $term = new Term();
+
+        //hydrate toutes les propriétés
+        //(donner une valeur)
+        $term->setSlug('poupoutine2');
+        $term->setTerm('Poupoutine2');
+        $term->setOrigin('dlafjdkl 2 2  2 ');
+        $term->setIsPublished(true);
+        $term->setDateCreated(new \DateTime());
+
+        //on sauvegarde cette instance svp !
+        $entityManager->persist($term);
+
+        //et on exécute la requête maintenant
+        $entityManager->flush();
+
+        //pour supprimer
+        //$entityManager->remove($term);
+        //$entityManager->flush();
+
+        die("ok");
+    }
+
 }
---
+++ src/Entity/Term.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TermRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TermRepository::class)
+ * @ORM\Table(indexes={@ORM\Index(name="slug_idx", columns={"slug"})})
+ */
+class Term
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $term;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $pronunciation;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $variations;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $origin;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $nota_bene;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $usage_precision;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_published;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_created;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_updated;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getTerm(): ?string
+    {
+        return $this->term;
+    }
+
+    public function setTerm(string $term): self
+    {
+        $this->term = $term;
+
+        return $this;
+    }
+
+    public function getPronunciation(): ?string
+    {
+        return $this->pronunciation;
+    }
+
+    public function setPronunciation(?string $pronunciation): self
+    {
+        $this->pronunciation = $pronunciation;
+
+        return $this;
+    }
+
+    public function getVariations(): ?string
+    {
+        return $this->variations;
+    }
+
+    public function setVariations(?string $variations): self
+    {
+        $this->variations = $variations;
+
+        return $this;
+    }
+
+    public function getOrigin(): ?string
+    {
+        return $this->origin;
+    }
+
+    public function setOrigin(?string $origin): self
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+
+    public function getNotaBene(): ?string
+    {
+        return $this->nota_bene;
+    }
+
+    public function setNotaBene(?string $nota_bene): self
+    {
+        $this->nota_bene = $nota_bene;
+
+        return $this;
+    }
+
+    public function getUsagePrecision(): ?string
+    {
+        return $this->usage_precision;
+    }
+
+    public function setUsagePrecision(?string $usage_precision): self
+    {
+        $this->usage_precision = $usage_precision;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->is_published;
+    }
+
+    public function setIsPublished(bool $is_published): self
+    {
+        $this->is_published = $is_published;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->date_created;
+    }
+
+    public function setDateCreated(\DateTimeInterface $date_created): self
+    {
+        $this->date_created = $date_created;
+
+        return $this;
+    }
+
+    public function getDateUpdated(): ?\DateTimeInterface
+    {
+        return $this->date_updated;
+    }
+
+    public function setDateUpdated(?\DateTimeInterface $date_updated): self
+    {
+        $this->date_updated = $date_updated;
+
+        return $this;
+    }
+}
